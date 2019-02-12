d59dcbe6ac4121653b834681aa2ec5f540d98867
McLemoreT <35839850+McLemoreT@users.noreply.github.com>
2019-02-12 17:39:50 -0500

    Initial Nation upload
    Contains information about the nation that is being simulated

---
+++ Nation.java
@@ -0,0 +1,12 @@
+public class Nation {
+	public int Consumer_Goods = 0;
+	public int Military_Factories = 0;
+	public int Civilian_Factories = 0;
+	public double Construction_Bonus = 0;
+	public double Civilian_Bonus = 0;
+	//Constructors
+	public SetConsumerGoods (int a){
+		Consumer_Goods = a;
+	}
+	public SetMilitaryFactories (int a){
+		Military_Factories = a
