18621f556c991f96eb75177171e08f72257daab4
McLemoreT <35839850+McLemoreT@users.noreply.github.com>
2019-02-12 17:41:00 -0500

    Added remaining constructors

--- Nation.java
+++ Nation.java
@@ -2,6 +2,7 @@ public class Nation {
 	public int Consumer_Goods = 0;
 	public int Military_Factories = 0;
 	public int Civilian_Factories = 0;
+	public int Naval_Dockyards
 	public double Construction_Bonus = 0;
 	public double Civilian_Bonus = 0;
 	//Constructors
@@ -9,4 +10,22 @@ public class Nation {
 		Consumer_Goods = a;
 	}
 	public SetMilitaryFactories (int a){
-		Military_Factories = a
+		Military_Factories = a;
+	}
+	public SetCivilianFactories (int a){
+		Civilian_Factories = a;
+	}
+	public SetNavalDockyards (int a){
+		Naval_Dockyards = a;
+	}
+	public SetConstructionBonus (double a){
+		Construction_Bonus = a;
+	}
+	public SetCivilianBonus (double a){
+		Civilian_Bonus = a;
+	}
+	//Return functions
+	public int GetConsumerGoods(){
+		return Consumer_Goods;
+	}
+	public int GetMilitaryFactories
